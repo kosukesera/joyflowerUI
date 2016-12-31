61a60b081220456516e4150515d5cb0637d7ad6d
Kosuke Sera <k0414sailor@gmail.com>
2017-01-01 06:28:18 +0900

    'First'

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GameDirector : MonoBehaviour {
+
+    private int num_obj;
+    public Text ObjText;
+    //    private ObjImg;
+    //public Canvas canvas;
+
+    void SetNum(int num)
+    {
+        num_obj = num;
+    }
+
+    int GetNum(){
+     return num_obj;
+    }
+
+    // Use this for initialization
+    void Start () {
+        num_obj = 0;
+
+        ObjText.text = "aaa";
+
+    }
+
+    void SetUI(int num)
+    {
+        ObjText.text = num.ToString();
+    }
+
+    public void CountObj()
+    {
+        int num = 0;
+        num = GetNum();
+        SetNum(num + 1);
+        SetUI(num + 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SphereController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 左クリックしたオブジェクトを取得する関数(3D)
+
+public class SphereController : MonoBehaviour {
+
+    public GameObject getClickObject()
+    {
+        GameObject result = null;
+        // 左クリックされた場所のオブジェクトを取得
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                result = hit.collider.gameObject;
+            }
+        }
+        return result;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject obj = getClickObject();
+        if (obj != null)
+        {
+            // 以下オブジェクトがクリックされた時の処理
+            Destroy(gameObject);
+
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().CountObj();
+
+        }
+    }
+}
